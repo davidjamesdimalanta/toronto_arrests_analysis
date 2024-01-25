059e636259672264f77908d4632705c2cdba2999
David James <123336283+davidjamesdimalanta@users.noreply.github.com>
2024-01-25 00:30:55 -0500

    push tests to repo

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -14,26 +14,35 @@ library(knitr)
 library(ggplot2)
 library(testthat)
 
-# plot arrest count by general location
-ggplot(summarized_data,
-       aes(x = factor(ward, levels = c(
-         "Ward 1", "Ward 5", "Ward 7",
-         "Ward 13", "Ward 16", "Ward 18",
-         "Ward 20", "Ward 21", "Ward 22",
-         "Ward 24", "Ward 2", "Ward 3", "Ward 4",
-         "Ward 6", "Ward 8", "Ward 9", "Ward 10",
-         "Ward 11", "Ward 12", "Ward 14",
-         "Ward 15", "Ward 17", "Ward 19",
-         "Ward 23", "Ward 25",
-         "Outer Toronto Ward"
-       )), y = arrest_count)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Toronto Arrest Count by General Location",
-       x = "Neighbourhood", y = "Arrest Count")
-theme(axis.text.x = element_text(size = 8, angle = 45, hjust = 1))
+set.seed(123)
 
-# plot census data
-census_data <- read_csv("outputs/data/census_data.csv")
-ward_hh_income <- census_data |>
-  slice(1:26)
-kable(census_data, format = "pipe", caption = "Toronto Census Data by Ward")
+# simulating data for Arrest Count by Ward
+num_wards <- 25
+low_arrest_count <- 5000
+high_arrest_count <- 65000
+
+sim_arrest_data <-
+  tibble(
+    sim_arrest_count =
+    runif(n = num_wards, min = low_arrest_count, max = high_arrest_count),
+    noise = rnorm(n = num_wards, mean = 0, sd = 3)
+  ) |>
+  select(-noise)
+
+print(sim_arrest_data, n = 25)
+
+
+# simulating data for Average Household income by Ward
+num_wards <- 25
+low_hh_income <- 5000
+high_hh_income <- 65000
+
+sim_hh_data <-
+  tibble(
+    sim_hh_income =
+    runif(n = num_wards, min = low_hh_income, max = high_hh_income),
+    noise = rnorm(n = num_wards, mean = 0, sd = 3)
+  ) |>
+  select(-noise)
+
+print(sim_hh_data, n = 25)
